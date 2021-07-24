d4287852904930c94c2765123c7dc4c9b60264b9
Guy Geva <guy1030@gmail.com>
2021-07-24 17:47:22 +0300

    Fix

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://misterfy:1234@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://misterfy:1234@cluster0-klgzh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
